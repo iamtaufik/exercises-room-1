24da2e4f9291266bb53dcd61cb52a8724c4c1d48
dzachmidz20 <fahmialhafidza@gmail.com>
2023-09-27 21:34:35 +0700

    feat: menambahkan index dan show

--- 2_node-postgre/database/postgres.js
+++ 2_node-postgre/database/postgres.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'tatangdev',
+    user: 'postgres',
     database: 'new_blog',
-    password: '',
+    password: 'MIfahmi20',
     port: 5432,
     host: 'localhost'
 });
