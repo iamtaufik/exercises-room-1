a01ed9beae488cb103a601df75c8a4c446dad856
iamtaufik <muhtopik07@gmail.com>
2023-09-27 22:03:04 +0700

    fix: add some validation

--- 2_node-postgre/helpers/crud.js
+++ 2_node-postgre/helpers/crud.js
@@ -1,13 +1,8 @@
-const fs = require('fs');
-const posts = require('../database/posts');
-const PostModel = require('../models/post');
 const pool = require('../database/postgres');
 
 function create(title, body) {
     return new Promise(async (resolve, reject) => {
         try {
-            // "SELECT * FROM posts where id = $1", [1]
-            // "UPDATE posts SET title = $1, body = $2", ["title", "body"]
             let result = await pool.query("INSERT INTO posts (title, body) values ($1, $2) RETURNING *;", [title, body]);
             resolve(result.rows[0]);
         } catch (err) {
@@ -30,6 +25,7 @@ function show(id) {
     return new Promise(async (resolve, reject) => {
       try {
         let result = await pool.query("SELECT * FROM posts WHERE id = $1;", [id]);
+        if(!result.rowCount) return reject(`post with id ${id} not found!`);
         resolve(result.rows[0]);
       } catch (err) {
         return reject(err);
@@ -41,6 +37,7 @@ function update(id, title, body) {
     return new Promise(async (resolve, reject) => {
         try {
             const result = await pool.query("UPDATE posts SET title = $1, body = $2 WHERE id = $3 RETURNING *;", [title, body, id]);
+            if(!result.rowCount) return reject(`post with id ${id} not found!`);
             resolve(result.rows[0]);
         } catch (error) {
             reject(error);
@@ -52,8 +49,6 @@ function update(id, title, body) {
 function destroy(id) {
     return new Promise(async (resolve, reject) => {
         try {
-            // "SELECT * FROM posts where id = $1", [1]
-            // "UPDATE posts SET title = $1, body = $2", ["title", "body"]
             let result = await pool.query("DELETE from posts where id = $1 RETURNING *;", [1]);
             if (result.rows.length == 0) return reject (`post with id ${id} not found!`)
             resolve(`posts with id ${id} is deleted!`);
